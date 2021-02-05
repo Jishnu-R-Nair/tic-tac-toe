a48bcc08b6de4d7dc7a47d282b61f442e9771e49
Jishnu R Nair <rnair.jishnu@gmail.com>
2021-02-05 11:41:19 +0530

    made basic functionality working

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,6 +2,26 @@ import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
 function Square({ value, onClick }) {
   return (
     <button className='square' onClick={onClick}>
@@ -12,14 +32,27 @@ function Square({ value, onClick }) {
 
 function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
+  const [xNext, setXNext] = useState(true);
 
   const handleClick = (i) => {
-    let updatedSquares = [].concat(squares);
-    updatedSquares[i] = 'X';
+    let updatedSquares = squares.slice();
+
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+
+    updatedSquares[i] = xNext ? 'X' : 'O';
     setSquares(updatedSquares);
+    setXNext(!xNext);
   };
 
-  const status = 'Next player: X';
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = 'Winner: ' + winner;
+  } else {
+    status = 'Next player: ' + (xNext ? 'X' : 'O');
+  }
 
   return (
     <div>
